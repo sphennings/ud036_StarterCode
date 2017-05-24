bce80ed55c02f6362db43747046144dc1184e0a3
Ben Stephens <ben.stephens.programmer@gmail.com>
2017-05-23 23:23:55 -0400

    Small tweaks to website generation

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -9,7 +9,7 @@ main_page_head = '''
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+    <title>My Favorite Movies!</title>
 
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
@@ -107,7 +107,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">My Favorite Movies</a>
           </div>
         </div>
       </div>
